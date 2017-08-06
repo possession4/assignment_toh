3af467d6e1da004ceecf62fffe8b750bb0e9d0ab
possession4 <himebrittny@hotmail.com>
2017-08-06 15:28:33 -0400

    More pseudocode

--- towers.rb
+++ towers.rb
@@ -1,20 +1,22 @@
-Class TowerOfHanoi
-  @height_of_tower = 0
+class TowerOfHanoi
+  @@height_of_tower = 0
+  @current_board << [0,0,0]
 
   def initialize(height_of_tower)
-    @height_of_tower = height_of_tower
+    @@height_of_tower = height_of_tower
+    update_current_board()
   end
 
   def play
     print instructions
     render_current_board
     print "Enter Move >"
-    input = gets.strip
+    input = check_input(gets.strip)
     unless (input == "q" || win?)
-    
-    else
-      render_current_board
-      "You win!"
+        render_current_board
+        print "You win!"
+      else
+        
     end
   end 
 
@@ -22,15 +24,21 @@ Class TowerOfHanoi
 
   end
 
-  def win?
+  def update_current_board
+    
+  end
 
+  def win?
+    @current_board.each do |b|
+      
+    end
   end
 
   def instructions
     puts "Welcome to Tower of Hanoi!\nInstructions:\nEnter where you'd like to move from and to\nin the format '1,3'. Enter 'q' to quit."
   end
 
-  def check_input
-
+  def check_input(input)
+    #  input.format == %i,%i
   end
 end
